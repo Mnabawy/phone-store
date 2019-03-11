87a2d501a942a22650faddb8c81b95e9b0c86a18
Mnabawy <hassanm486@yahoo.com>
2019-03-11 10:30:00 +0200

    e commerc web application

---
+++ src/components/Navbar.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import styled from 'styled-components'
+import { ButtonContainer } from './Button';
+
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <NavWrapper className="navbar navbar-expand-sm navbar-dark  bg-primary px-sm-5">
+        {/*
+            https://www.iconfinder.com/icons/1243689/call_phone_icon
+            Creative Commons (Attribution 3.0 Unported);
+            https://www.iconfinder.com/Makoto_msk  */}
+
+        <Link to="/">
+          <img src={logo} alt="logo" className="navbar-brand" />
+        </Link>
+
+
+        <ul className="navbar-nav align-items-center">
+          <li className="nav-item ml-5"></li>
+
+          <li>
+            <Link to="/" className="nav-link" >Products
+          </Link>
+          </li>
+
+        </ul>
+        <Link to="/cart" className="ml-auto">
+          <ButtonContainer>
+            <span >
+
+              <i className="fas fa-cart-plus" />my cart
+          </span>
+          </ButtonContainer>
+        </Link>
+      </NavWrapper>
+    );
+  }
+}
+
+
+const NavWrapper = styled.nav`
+background:var(--mainblue);
+.nav-link{
+  color:var(--mainwhite)!important;
+  font-size:1.3rem;
+  text-transform: capitalize!important;
+}
+`;
---
+++ src/components/ProductList.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+import {ProductConsumer} from './context';
+import Product from './Product';
+import Title from './Title';
+
+
+class ProductList extends Component {
+
+ render() { 
+  return ( 
+<React.Fragment>
+  <div className="pt-5 pb-5">
+    <div className="container ">
+          <Title name="our" title="products" />
+              <div className="row">
+              <ProductConsumer>
+                  {value=>{
+                  return value.products.map(product =>{
+                  return <Product key={product.id} product={product} />;
+            });
+          }}
+          </ProductConsumer>
+      </div>
+    </div>     
+  </div>
+</React.Fragment>  
+   );
+ }
+}
+ 
+export default ProductList;
